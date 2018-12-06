462608e31cd37a49b4e2f504faed03e1ce906922
Arturs Klezovics <klezovich@gmail.com>
2018-12-06 11:13:47 +0000

    Initial commit

---
+++ src/test/java/com/klezovich/active_mq_test/SpringActiveMqTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.klezovich.active_mq_test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringActiveMqTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
