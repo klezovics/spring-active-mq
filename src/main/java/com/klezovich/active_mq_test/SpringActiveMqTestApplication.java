d8c96c3608799f45468fa7c571913336a466e712
Arturs Klezovics <klezovich@gmail.com>
2018-12-06 12:11:10 +0000

    Add implementation

--- src/main/java/com/klezovich/active_mq_test/SpringActiveMqTestApplication.java
+++ src/main/java/com/klezovich/active_mq_test/SpringActiveMqTestApplication.java
@@ -2,8 +2,10 @@ package com.klezovich.active_mq_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class SpringActiveMqTestApplication {
 
 	public static void main(String[] args) {
